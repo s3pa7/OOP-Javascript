20db98db0e16e8a2f9570b53350b31051fb73bd7
s3pa7 <s3pa7@abv.bg>
2016-08-24 22:07:50 +0300

    First OOP

--- assets/js/GSM.js
+++ assets/js/GSM.js
@@ -11,51 +11,50 @@ function GSM(model) {
         return _model;
     };
 
-    this.setModel = function (modelSet) {
-        _model = modelSet;
+    this.setModel = function (setModel) {
+        _model = setModel;
     };
 
     this.getHasSimCard = function () {
         return _hasSimCard;
     };
 
-    this.setHasSimCard = function (hasSimCardSet) {
-        _hasSimCard = hasSimCardSet;
+    this.setHasSimCard = function (setSimCard) {
+        _hasSimCard = setSimCard;
     };
 
     this.getSimMobileNumber = function () {
         return _simMobileNumber;
     };
 
-    this.setSimMobileNumber = function (simMobileNumberSet) {
-        _simMobileNumber = simMobileNumberSet;
+    this.setSimMobileNumber = function (setSimMobilNumber) {
+        _simMobileNumber = setSimMobilNumber;
     };
 
     this.getOutgoingCallsDuration = function () {
         return _outgoingCallsDuration;
     };
 
-    this.setOutgoingCallsDuration = function (outgoingCallsDurationSet) {
-        _outgoingCallsDuration = outgoingCallsDurationSet;
+    this.setOutgoingCallsDuration = function (setOutGoingCallsDuration) {
+        _outgoingCallsDuration = setOutGoingCallsDuration;
     };
 
     this.getLastIncomingCall = function () {
         return _lastIncomingCall;
     };
 
-    this.setLastIncomingCall = function (lastIncomingCallSet) {
-        _lastIncomingCall = lastIncomingCallSet;
+    this.setLastIncomingCall = function (setLastIncomingCall) {
+        _lastIncomingCall = setLastIncomingCall;
     };
 
     this.getLastOutgoingCall = function () {
         return _lastOutgoingCall;
     };
 
-    this.setLastOutgoingCall = function (lastOutgoingCallSet) {
-        _lastOutgoingCall = lastOutgoingCallSet;
+    this.setLastOutgoingCall = function (setLastOutgoingCall) {
+        _lastOutgoingCall = setLastOutgoingCall;
     };
 }
-
 
 GSM.prototype.insertSimCard = function (simMobileNumber) {
     var myRegexp = /08[789]\d{7}/;
@@ -69,23 +68,6 @@ GSM.prototype.insertSimCard = function (simMobileNumber) {
 
 GSM.prototype.removeSimCard = function () {
     this.setHasSimCard(false);
-}
-
-GSM.prototype.call = function (objectGSM, objectCall) {
-	debugger;
-    if ((this.getHasSimCard() == true) && (objectGSM.getHasSimCard() == true)) {
-        if (this.getSimMobileNumber() == objectGSM.getSimMobileNumber()) {
-            console.log("ednakvi nomera");
-            return false;
-        } else if (!objectCall.getDuration()) {
-            throw new Error("The duration must be between 0 and 120.");
-        }
-    }
-
-    this.setLastOutgoingCall(objectCall.getDuration());
-    objectGSM.setLastIncomingCall(objectCall.getDuration());
-    this.setOutgoingCallsDuration(this.getOutgoingCallsDuration() + this.getLastOutgoingCall());
-
 }
 
 GSM.prototype.outgoingCallsDuration = function () {
@@ -102,14 +84,31 @@ GSM.prototype.printInfoForTheLastOutgoingCall = function () {
 
 GSM.prototype.printInfoForTheLastIncomingCall = function () {
     return "Duration of last incoming call: " + this.getLastIncomingCall();
-   
 }
+
 GSM.prototype.getInfo = function () {
-    console.log("Does " + this.getModel() + " have SIM card: " + this.getHasSimCard());
+    var flag = this.getHasSimCard() ? "yes" : "no";
+    console.log("Does " + this.getModel() + " have SIM card: " + flag);
     console.log(this.getModel() + "'s mobile number is: " + this.getSimMobileNumber());
-    console.log(this.getModel() + "'s last outgoing call's duration is: " + this.getLastOutgoingCall());
-    console.log(this.getModel() + "'s last incoming call's duration is: " + this.getLastIncomingCall());
-    console.log(this.getModel() + "'s sum for all outgoing calls so far is: " + this.getSumForCall(call_));
+    if (this.getLastIncomingCall()) {
+        console.log(this.getModel() + "'s last incoming call's duration is: " + this.getLastIncomingCall() + " minutes");
+    }
+}
 
+GSM.prototype.call = function (objectGSM, duration) {
+    if ((this.getHasSimCard() == true) && (objectGSM.getHasSimCard() == true)) {
+        if (this.getSimMobileNumber() == objectGSM.getSimMobileNumber()) {
+            console.log("ednakvi nomera");
+            return false;
+        }
+    }
+    var call_ = new Call(this, objectGSM, duration);
+    this.setLastOutgoingCall(call_.getDuration());
+    objectGSM.setLastIncomingCall(call_.getDuration());
+    this.setOutgoingCallsDuration(this.getOutgoingCallsDuration() + this.getLastOutgoingCall());
 
+    console.log("The receiver of " + this.getModel() + "'s last outgoing call is: " + objectGSM.getSimMobileNumber());
+    this.getInfo();
+    console.log(this.getModel() + "'s last outgoing call's duration is: " + this.getLastOutgoingCall() + " minutes");
+    console.log(this.getModel() + "'s sum for all outgoing calls so far is: " + this.getSumForCall(call_) + " BGN");
 }
